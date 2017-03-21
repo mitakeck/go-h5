42250e6e0654217c73d4b4f04bf3d872fffde807
mitakeck <mitake.ck@gmail.com>
2017-03-21 22:40:26 +0900

    root field チャンク読み込み処理追加

--- reader.go
+++ reader.go
@@ -32,9 +32,13 @@ type Superblock struct {
 type Reader struct {
 	input    FileReader
 	fileName string
+
+	superblock Superblock
 }
 
-func (r *Reader) readChunk(size int) ([]byte, error) {
+func (r *Reader) readChunk(size int, offset int64) ([]byte, error) {
+	r.input.Seek(offset, os.SEEK_SET)
+
 	chunk := make([]byte, size)
 	if err := binary.Read(r.input, binary.BigEndian, chunk); err != nil {
 		return nil, err
@@ -42,23 +46,69 @@ func (r *Reader) readChunk(size int) ([]byte, error) {
 	return chunk, nil
 }
 
-// parse format signature field
-func (r *Reader) parseSignature() error {
+// parse Superblock field
+func (r *Reader) parseSuperblock() error {
 	// Decimal:            137   72   68   70   13   10    26   10
 	// Hexadecimal:         89   48   44   46   0d   0a    1a   0a
 	// ASCII C Notation:  \211    H    D    F   \r   \n  \032   \n
 	// https://support.hdfgroup.org/HDF5/doc/H5.format.html
 
 	// get format signature field
-	chunk, err := r.readChunk(signatureSize)
+	chunk, err := r.readChunk(signatureSize, 0)
 	if err != nil {
-		return nil
+		return err
 	}
 
 	// check format signature field
 	if bytes.Compare(chunk, signatureValue) != 0 {
 		return fmt.Errorf("Input File is Not hdf5 Format : %s", r.fileName)
 	}
+
+	var hoge int
+
+	// get version number of the Superblock
+	chunk, err = r.readChunk(2, 8)
+	if err != nil {
+		return err
+	}
+	binary.Read(bytes.NewBuffer(chunk), binary.BigEndian, &hoge)
+	r.superblock.SuperblockVersin = hoge
+
+	// get version number of file's free space storage
+	chunk, err = r.readChunk(2, 10)
+	if err != nil {
+		return err
+	}
+	binary.Read(bytes.NewBuffer(chunk), binary.BigEndian, &r.superblock.FreeSpaceInformationVersion)
+
+	// get version number of root group symbol table entry
+	chunk, err = r.readChunk(2, 12)
+	if err != nil {
+		return err
+	}
+	binary.Read(bytes.NewBuffer(chunk), binary.BigEndian, &r.superblock.RootGroupSymbolTableEntryVersion)
+
+	// get version number of share header mesage format
+	chunk, err = r.readChunk(2, 16)
+	if err != nil {
+		return err
+	}
+	binary.Read(bytes.NewBuffer(chunk), binary.BigEndian, &r.superblock.SharedHeaderMessageFormatVersion)
+
+	// get size of offsets
+	chunk, err = r.readChunk(2, 18)
+	if err != nil {
+		return err
+	}
+	binary.Read(bytes.NewBuffer(chunk), binary.BigEndian, &r.superblock.offset)
+
+	// get size of lengths
+	chunk, err = r.readChunk(2, 20)
+	if err != nil {
+		return err
+	}
+	binary.Read(bytes.NewBuffer(chunk), binary.BigEndian, &hoge)
+	r.superblock.length = hoge
 
 	return nil
 }
@@ -88,7 +138,7 @@ func newH5Reader(fileName string) (*Reader, error) {
 	reader.input = bytes.NewReader(data)
 
 	// check format signature
-	if err := reader.parseSignature(); err != nil {
+	if err := reader.parseSuperblock(); err != nil {
 		return reader, err
 	}
 
